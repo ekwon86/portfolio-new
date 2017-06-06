70fc0b2a612565542ff5a11f5a834374eb577831
Eugene Kwon <ekwon86@gmail.com>
2017-06-05 18:53:53 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <landing></landing>
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'landing': Landing
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="landing">
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .landing {
+    height: 100vh;
+    background-image: url('../assets/bg.jpg');
+    background-position: center;
+    background-size: 100% 100%;
+  }
+</style>
